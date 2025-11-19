101b42abdd2390cc2cf70080964d9020bd69d90a
k0s5 <jobs.koss@gmail.com>
2025-11-19 21:09:55 +0300

    feat: implement custom JSON.stringify to handle BigInt serialization

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,41 @@ import { AppModule } from '@core/app/app.module'
 import { ConfigService } from '@nestjs/config'
 import { EnvironmentVariables } from '@config'
 
+const overrideJsonBigIntSerialization = (): void => {
+  const originalJSONStringify = JSON.stringify
+
+  JSON.stringify = function (value: any, replacer, space: number): string {
+    const bigIntReplacer = (_key: string, value: any): any => {
+      if (typeof value === 'bigint') {
+        return parseInt(value.toString())
+      }
+      return value
+    }
+
+    const customReplacer = (key: string, value: any): any => {
+      if (Array.isArray(replacer) && !replacer.includes(key) && key !== '') {
+        return undefined
+      }
+
+      const modifiedValue = bigIntReplacer(key, value)
+
+      if (typeof replacer === 'function') {
+        return replacer(key, modifiedValue)
+      }
+
+      return modifiedValue
+    }
+
+    return originalJSONStringify(
+      value,
+      replacer != null ? customReplacer : bigIntReplacer,
+      space
+    )
+  }
+}
+
+overrideJsonBigIntSerialization()
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
